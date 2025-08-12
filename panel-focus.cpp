4d60e77e92fcb351695bb7658847475dc1650fe7
Scott Moreau <oreaus@gmail.com>
2025-08-11 18:19:59 -0600

    Initial commit

---
+++ panel-focus.cpp
@@ -0,0 +1,92 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2025 Scott Moreau <oreaus@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <wayfire/plugin.hpp>
+#include <wayfire/toplevel-view.hpp>
+#include <wayfire/view-helpers.hpp>
+#include <wayfire/plugins/ipc/ipc-activator.hpp>
+
+
+namespace wf
+{
+class wayfire_panel_focus : public wf::plugin_interface_t
+{
+    wf::ipc_activator_t cycle{"panel-focus/cycle"};
+    wayfire_view current_focus_view;
+
+  public:
+
+    void init() override
+    {
+        cycle.set_handler(cycle_panels);
+        current_focus_view = wf::get_core().seat->get_active_view();
+    }
+
+    wf::ipc_activator_t::handler_t cycle_panels = [=] (wf::output_t *output, wayfire_view)
+    {
+        wayfire_view last_focus_view = current_focus_view;
+        bool focus_view_found = false;
+        for (auto& view : wf::get_core().get_all_views())
+        {
+            if (view == current_focus_view && !focus_view_found)
+            {
+                focus_view_found = true;
+                continue;
+            }
+            if (view->role == wf::VIEW_ROLE_DESKTOP_ENVIRONMENT &&
+                wf::get_view_layer(view) > wf::scene::layer::WORKSPACE &&
+                focus_view_found)
+            {
+                wf::get_core().seat->focus_view(view);
+                current_focus_view = view;
+                break;
+            }
+        }
+        if (last_focus_view == current_focus_view)
+        {
+            for (auto& view : wf::get_core().get_all_views())
+            {
+                if (view->role == wf::VIEW_ROLE_DESKTOP_ENVIRONMENT &&
+                    wf::get_view_layer(view) > wf::scene::layer::WORKSPACE)
+                {
+                    wf::get_core().seat->focus_view(view);
+                    current_focus_view = view;
+                    break;
+                }
+            }
+        }
+        if (last_focus_view == current_focus_view)
+        {
+            LOGI("Failed to cycle views!");
+        }
+        return true;
+    };
+
+    void fini() override
+    {
+    }
+};
+}
+
+DECLARE_WAYFIRE_PLUGIN(wf::wayfire_panel_focus);
